eae9b1925b4338ea9d2faafed15287a0e82c02bb
Chris Tsang <tyt2y7@gmail.com>
2019-08-12 20:29:48 +0800

    Refactor code into 'main' and 'universe'

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,16 @@
-extern crate cfg_if;
 extern crate wasm_bindgen;
 extern crate web_sys;
 
 mod utils;
+mod universe;
+
+use utils::*;
+use universe::*;
 
 use std::rc::Rc;
 use std::cell::RefCell;
-use wasm_bindgen::prelude::*;
 use wasm_bindgen::JsCast;
+use wasm_bindgen::prelude::*;
 
 struct Fps {
     frames: Vec<f64>,
@@ -44,275 +47,6 @@ max of last 100 = {:.0}
         }
         self.last_frame_time_stamp = now;
     }
-}
-
-#[repr(u8)]
-#[derive(Clone, Copy, Debug, PartialEq, Eq)]
-pub enum Cell {
-    Dead = 0,
-    Alive = 1,
-}
-
-impl Cell {
-    fn toggle(&mut self) {
-        *self = match *self {
-            Cell::Dead => Cell::Alive,
-            Cell::Alive => Cell::Dead,
-        };
-    }
-}
-
-pub struct Universe {
-    width: u32,
-    height: u32,
-    cells: Vec<Cell>,
-}
-
-impl Universe {
-
-    const CELL_SIZE: u32 = 5; // px
-    const GRID_COLOR: &'static str = "#CCCCCC";
-    const DEAD_COLOR: &'static str = "#FFFFFF";
-    const ALIVE_COLOR: &'static str = "#000000";
-
-    fn get_index(&self, row: u32, column: u32) -> usize {
-        (row * self.width + column) as usize
-    }
-
-    /// Get the dead and alive values of the entire universe.
-    pub fn get_cells(&self) -> &[Cell] {
-        &self.cells
-    }
-
-    /// Set cells to be alive in a universe by passing the row and column
-    /// of each cell as an array.
-    pub fn set_cells(&mut self, cells: &[(u32, u32)]) {
-        for (row, col) in cells.iter().cloned() {
-            let idx = self.get_index(row, col);
-            self.cells[idx] = Cell::Alive;
-        }
-    }
-
-    fn live_neighbor_count(&self, row: u32, column: u32) -> u8 {
-        let mut count = 0;
-
-        let north = if row == 0 {
-            self.height - 1
-        } else {
-            row - 1
-        };
-
-        let south = if row == self.height - 1 {
-            0
-        } else {
-            row + 1
-        };
-
-        let west = if column == 0 {
-            self.width - 1
-        } else {
-            column - 1
-        };
-
-        let east = if column == self.width - 1 {
-            0
-        } else {
-            column + 1
-        };
-
-        let nw = self.get_index(north, west);
-        count += self.cells[nw] as u8;
-
-        let n = self.get_index(north, column);
-        count += self.cells[n] as u8;
-
-        let ne = self.get_index(north, east);
-        count += self.cells[ne] as u8;
-
-        let w = self.get_index(row, west);
-        count += self.cells[w] as u8;
-
-        let e = self.get_index(row, east);
-        count += self.cells[e] as u8;
-
-        let sw = self.get_index(south, west);
-        count += self.cells[sw] as u8;
-
-        let s = self.get_index(south, column);
-        count += self.cells[s] as u8;
-
-        let se = self.get_index(south, east);
-        count += self.cells[se] as u8;
-
-        count
-    }
-
-    pub fn tick(&mut self) {
-        // let _timer = Timer::new("Universe::tick");
-
-        let mut next = self.cells.clone();
-
-        for row in 0..self.height {
-            for col in 0..self.width {
-                let idx = self.get_index(row, col);
-                let cell = self.cells[idx];
-                let live_neighbors = self.live_neighbor_count(row, col);
-
-                let next_cell = match (cell, live_neighbors) {
-                    // Rule 1: Any live cell with fewer than two live neighbours
-                    // dies, as if caused by underpopulation.
-                    (Cell::Alive, x) if x < 2 => Cell::Dead,
-                    // Rule 2: Any live cell with two or three live neighbours
-                    // lives on to the next generation.
-                    (Cell::Alive, 2) | (Cell::Alive, 3) => Cell::Alive,
-                    // Rule 3: Any live cell with more than three live
-                    // neighbours dies, as if by overpopulation.
-                    (Cell::Alive, x) if x > 3 => Cell::Dead,
-                    // Rule 4: Any dead cell with exactly three live neighbours
-                    // becomes a live cell, as if by reproduction.
-                    (Cell::Dead, 3) => Cell::Alive,
-                    // All other cells remain in the same state.
-                    (otherwise, _) => otherwise,
-                };
-
-                next[idx] = next_cell;
-            }
-        }
-
-        self.cells = next;
-    }
-
-    pub fn new() -> Universe {
-        utils::set_panic_hook();
-
-        let width = 128;
-        let height = 128;
-
-        let cells = (0..width * height)
-            .map(|i| {
-                if i % 2 == 0 || i % 7 == 0 {
-                    Cell::Alive
-                } else {
-                    Cell::Dead
-                }
-            })
-            .collect();
-
-        Universe {
-            width,
-            height,
-            cells,
-        }
-    }
-
-    pub fn width(&self) -> u32 {
-        self.width
-    }
-
-    /// Set the width of the universe.
-    ///
-    /// Resets all cells to the dead state.
-    pub fn set_width(&mut self, width: u32) {
-        self.width = width;
-        self.cells = (0..width * self.height).map(|_i| Cell::Dead).collect();
-    }
-
-    pub fn height(&self) -> u32 {
-        self.height
-    }
-
-    /// Set the height of the universe.
-    ///
-    /// Resets all cells to the dead state.
-    pub fn set_height(&mut self, height: u32) {
-        self.height = height;
-        self.cells = (0..self.width * height).map(|_i| Cell::Dead).collect();
-    }
-
-    pub fn cells(&self) -> *const Cell {
-        self.cells.as_ptr()
-    }
-
-    pub fn toggle_cell(&mut self, row: u32, column: u32) {
-        let idx = self.get_index(row, column);
-        self.cells[idx].toggle();
-    }
-
-    pub fn init_canvas(&self, canvas: &web_sys::HtmlCanvasElement) {
-        canvas.set_width((Self::CELL_SIZE + 1) * self.width + 1);
-        canvas.set_height((Self::CELL_SIZE + 1) * self.height + 1);
-    }
-
-    pub fn draw_grid(&self, ctx: &web_sys::CanvasRenderingContext2d) {
-        ctx.set_stroke_style(JsValue::from_str(Self::GRID_COLOR).as_ref());
-        ctx.begin_path();
-
-        // Vertical lines.
-        for i in 0..self.width {
-            ctx.move_to((i * (Self::CELL_SIZE + 1) + 1) as f64, 0.0);
-            ctx.line_to((i * (Self::CELL_SIZE + 1) + 1) as f64, ((Self::CELL_SIZE + 1) * self.height + 1) as f64);
-        }
-
-        // Horizontal lines.
-        for j in 0..self.height {
-            ctx.move_to(0.0, (j * (Self::CELL_SIZE + 1) + 1) as f64);
-            ctx.line_to(((Self::CELL_SIZE + 1) * self.width + 1) as f64, (j * (Self::CELL_SIZE + 1) + 1) as f64);
-        }
-
-        ctx.stroke();
-    }
-
-    pub fn draw_cells(&self, ctx: &web_sys::CanvasRenderingContext2d) {
-        // Alive cells.
-        ctx.set_fill_style(JsValue::from_str(Self::ALIVE_COLOR).as_ref());
-        for row in 0..self.height {
-            for col in 0..self.width {
-                let idx = self.get_index(row, col);
-                if self.cells[idx] != Cell::Alive {
-                    continue;
-                }
-
-                ctx.fill_rect(
-                    (col * (Self::CELL_SIZE + 1) + 1) as f64,
-                    (row * (Self::CELL_SIZE + 1) + 1) as f64,
-                    Self::CELL_SIZE as f64,
-                    Self::CELL_SIZE as f64
-                );
-            }
-        }
-
-        // Dead cells.
-        ctx.set_fill_style(JsValue::from_str(Self::DEAD_COLOR).as_ref());
-        for row in 0..self.height {
-            for col in 0..self.width {
-                let idx = self.get_index(row, col);
-                if self.cells[idx] != Cell::Dead {
-                    continue;
-                }
-
-                ctx.fill_rect(
-                    (col * (Self::CELL_SIZE + 1) + 1) as f64,
-                    (row * (Self::CELL_SIZE + 1) + 1) as f64,
-                    Self::CELL_SIZE as f64,
-                    Self::CELL_SIZE as f64
-                );
-            }
-        }
-    }
-}
-
-fn window() -> web_sys::Window {
-    web_sys::window().unwrap()
-}
-
-fn document() -> web_sys::Document {
-    window().document().unwrap()
-}
-
-fn request_animation_frame(f: &Closure<dyn FnMut()>) {
-    window()
-        .request_animation_frame(f.as_ref().unchecked_ref())
-        .expect("should register `requestAnimationFrame` OK");
 }
 
 #[wasm_bindgen(start)]
@@ -354,8 +88,8 @@ pub fn main() {
     let g = f.clone();
     let h = f.clone();
 
-    let universe_width = universe.width;
-    let universe_height = universe.height;
+    let universe_width = universe.width();
+    let universe_height = universe.height();
 
     universe.draw_grid(context.as_ref());
 
--- src/utils.rs
+++ src/utils.rs
@@ -1,6 +1,8 @@
-use cfg_if::cfg_if;
+extern crate cfg_if;
+extern crate wasm_bindgen;
+extern crate web_sys;
 
-cfg_if! {
+cfg_if::cfg_if! {
     // When the `console_error_panic_hook` feature is enabled, we can call the
     // `set_panic_hook` function to get better error messages if we ever panic.
     if #[cfg(feature = "console_error_panic_hook")] {
@@ -10,4 +12,19 @@ cfg_if! {
         #[inline]
         pub fn set_panic_hook() {}
     }
+}
+
+pub fn window() -> web_sys::Window {
+    web_sys::window().unwrap()
+}
+
+pub fn document() -> web_sys::Document {
+    window().document().unwrap()
+}
+
+pub fn request_animation_frame(f: &wasm_bindgen::prelude::Closure<dyn FnMut()>) {
+    use wasm_bindgen::JsCast;
+    window()
+        .request_animation_frame(f.as_ref().unchecked_ref())
+        .expect("should register `requestAnimationFrame` OK");
 }
